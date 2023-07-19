deb51fbcef46dc280f81f9cce7c65656b936facd
root <ygamboalh@gmail.com>
2023-07-19 18:59:39 +0000

    Desde digital oacen

--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  // host: env("HOST", "147.182.188.52"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
+  url: env("PUBLIC_URL", "https://siccic.com/backend/"),
+  // url: env("PUBLIC_URL", "http://147.182.188.52/backend/"),
 });
